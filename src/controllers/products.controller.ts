ce7de12cebe21f4373162d5b680531659304107b
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-12 21:50:01 -0600

    Clase 11 - Que es el metodo POST

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,19 +1,31 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
-  getProducts(
+  getAll(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products: limit => ${limit} - offset => ${offset} - brand => ${brand}`;
+    return {
+      message: `products: limit => ${limit} - offset => ${offset} - brand => ${brand}`,
+    };
   }
 
   //! las rutas dinámicas deben de ir al final
   @Get(':id')
-  getProduct(@Param('id') id: string) {
-    return `product ${id}`;
+  getOne(@Param('id') id: string) {
+    return {
+      meesage: `product ${id}`,
+    };
+  }
+
+  @Post()
+  createOne(@Body() payload: any) {
+    return {
+      message: 'Product created successfully',
+      payload,
+    };
   }
 }
