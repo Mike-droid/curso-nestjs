40cec24494fca9b23785cab0640f2ce56a22a63a
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-14 14:27:11 -0600

    category files

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,9 +1,51 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CategoriesService } from 'src/services/categories.service';
+import { CreateCategoryDto, UpdateCategoryDto } from 'src/dtos/category.dtos';
 
 @Controller('categories')
 export class CategoriesController {
-  @Get(':id/products/:productId')
-  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
-    return `category ${id} and product ${productId}`;
+  constructor(private categoriesService: CategoriesService) {} //* Esto creará una inyección de dependencias
+
+  @Get()
+  getAll(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return this.categoriesService.findAll();
+  }
+
+  //! las rutas dinámicas deben de ir al final
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.findOne(id);
+  }
+
+  @Post()
+  createOne(@Body() payload: CreateCategoryDto) {
+    return this.categoriesService.create(payload);
+  }
+
+  @Put(':id')
+  editOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.delete(id);
   }
 }
